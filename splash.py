1340d2b0f433c42f60a2e28a02ba48340865a372
jackscott1 <kieren.pinto@hotmail.com>
2019-01-24 14:44:14 +1100

    modify splash

--- splash.py
+++ splash.py
@@ -29,6 +29,7 @@ while entry == 1:
 
     try:
         print('Starting session' + str(session) + ' ...')
+        mywin.update()
         marmobox.run(taskname,mywin,limitTrial,animal_ID,session) #removed delay param
         mywin
         new = raw_input('Start new session? (y/n): ')
