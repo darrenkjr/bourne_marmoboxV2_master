570113bd5f02e34ed62449dfc8f7af448d02a84e
jackscott1 <kieren.pinto@hotmail.com>
2019-02-06 17:46:52 +1100

    minor changes

--- greenledtest.py
+++ greenledtest.py
@@ -4,15 +4,12 @@ import RPi.GPIO as GPIO
 import time
 
 def deliver():
-    while iterations <= 5
-        PIN_PUMP=13
-        PIN_BUZZER=10
-        PIN_LED_GREENB=18
+    iterations = 0
+    while iterations <= 5:
 
-        REWARD_VOLUME=500 #may require modification
-        REWARD_VOL_FACTOR=0.001 / 3
+        PIN_LED_GREEN=18
+
         BUZZER_LED_TIME=2.0
-        BUZZER_PITCH_CORRECT=800
 
         DEFAULT_FREQUENCY = 100		# In Hz
         DEFAULT_DUTYCYCLE = 50		# In percentage
@@ -26,9 +23,9 @@ def deliver():
     greenLED.start(DEFAULT_DUTYCYCLE)
     time.sleep(BUZZER_LED_TIME)
     greenLED.stop()
-    buzzer.stop()
 
     GPIO.cleanup()
+    iterations += 1
 
 
 if __name__ == '__main__':
