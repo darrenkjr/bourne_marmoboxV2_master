5df8c00e53fcd2644e7378a85f3dcbba0aef2ebd
jackscott1 <kieren.pinto@hotmail.com>
2019-02-07 10:35:57 +1100

    tinkering

--- ledtest.py
+++ ledtest.py
@@ -1,5 +1,5 @@
-#This is a manual reward module. This will only work on the PI not on PC. 
-# Do not import this module into any scripts. Always use marmocontrol which imports marmoio. 
+#This script tests LED function. If working, LEDs will flash every 15 seconds over 5 iterations. The buzzer will sound when the LEDs are meant to activate.
+
 import RPi.GPIO as GPIO
 import time
 
@@ -9,25 +9,27 @@ def deliver():
 
         PIN_BUZZER=10
         PIN_LED_GREEN=18
+        PIN_LED_BLUE=16
 
         BUZZER_LED_TIME=2.0
-        BUZZER_PITCH_CORRECT=800
+        BUZZER_PITCH_CORRECT=600
 
         DEFAULT_FREQUENCY = 100		# In Hz
         DEFAULT_DUTYCYCLE = 50		# In percentage
 
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
-        GPIO.setup(PIN_LED_GREEN, GPIO.OUT, initial=GPIO.LOW)
+       # GPIO.setup(PIN_LED_GREEN, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(PIN_LED_BLUE, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(PIN_BUZZER, GPIO.OUT, initial=GPIO.LOW)
 
         buzzer = GPIO.PWM(PIN_BUZZER, BUZZER_PITCH_CORRECT)
         buzzer.start(DEFAULT_DUTYCYCLE)
-        greenLED = GPIO.PWM(PIN_LED_GREEN, DEFAULT_FREQUENCY)
-        greenLED.start(DEFAULT_DUTYCYCLE)
+       # greenLED = GPIO.PWM(PIN_LED_GREEN, DEFAULT_FREQUENCY)
+        blueLED = GPIO.PWM(PIN_LED_BLUE, DEFAULT_FREQUENCY)
+        blueLED.start(DEFAULT_DUTYCYCLE)
+      #  greenLED.start(DEFAULT_DUTYCYCLE)
         time.sleep(BUZZER_LED_TIME)
-        greenLED.stop()
-        buzzer.stop()
 
         GPIO.cleanup()
         time.sleep(15)
