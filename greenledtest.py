c9bfe7e839a6e6b51e62f95aef9044ef4d2eb2d5
jackscott1 <kieren.pinto@hotmail.com>
2019-02-06 17:49:11 +1100

    include buzzer to indicate when led is activated

--- greenledtest.py
+++ greenledtest.py
@@ -7,9 +7,11 @@ def deliver():
     iterations = 0
     while iterations <= 5:
 
+        PIN_BUZZER=10
         PIN_LED_GREEN=18
 
         BUZZER_LED_TIME=2.0
+        BUZZER_PITCH_CORRECT=800
 
         DEFAULT_FREQUENCY = 100		# In Hz
         DEFAULT_DUTYCYCLE = 50		# In percentage
@@ -17,15 +19,18 @@ def deliver():
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
         GPIO.setup(PIN_LED_GREEN, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(PIN_BUZZER, GPIO.OUT, initial=GPIO.LOW)
 
-
-    greenLED = GPIO.PWM(PIN_LED_GREEN, DEFAULT_FREQUENCY)
-    greenLED.start(DEFAULT_DUTYCYCLE)
-    time.sleep(BUZZER_LED_TIME)
-    greenLED.stop()
+        buzzer = GPIO.PWM(PIN_BUZZER, BUZZER_PITCH_CORRECT)
+        buzzer.start(DEFAULT_DUTYCYCLE)
+        greenLED = GPIO.PWM(PIN_LED_GREEN, DEFAULT_FREQUENCY)
+        greenLED.start(DEFAULT_DUTYCYCLE)
+        time.sleep(BUZZER_LED_TIME)
+        greenLED.stop()
+        buzzer.stop()
 
     GPIO.cleanup()
-    iterations += 1
+    iterations +=1
 
 
 if __name__ == '__main__':
