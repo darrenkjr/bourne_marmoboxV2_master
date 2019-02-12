0d547bb4e6c369b92f15ceaf4c5a8555ef994e72
jackscott1 <kieren.pinto@hotmail.com>
2019-02-12 15:54:00 +1100

    tinkering

--- marmoio.py
+++ marmoio.py
@@ -37,7 +37,7 @@ class marmoIO:
     def Pump(self,state):
         GPIO.output(self.PIN_PUMP,GPIO.LOW if state else GPIO.HIGH)
         pumpa = GPIO.PWM(self.PIN_PUMP, 10000)
-        pumpa.start()
+        pumpa.start(5)
         time.sleep(2)
         GPIO.cleanup()
 
