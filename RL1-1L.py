24af3c4bfa815d2a9866ae0d5aeb63867483ca13
jackscott1 <kieren.pinto@hotmail.com>
2019-04-09 16:16:10 +1000

    testing

--- RL1-1L.py
+++ RL1-1L.py
@@ -262,7 +262,7 @@ def execTask(taskname,limitTrial,mywin, animal_ID,session):
     average_dist = float(df_results_clean[['Distance from reward center (px)']].mean())
     avg_reactiontime = float(df_results_clean[['Response latency (ms)']].mean())
     extra = limitTrial + total_outsides
-    total_accuracy = (limitTrial/extra)*100
+    total_accuracy = (10/extra)*100
 
     print(limitTrial)
     print(total_outsides)
