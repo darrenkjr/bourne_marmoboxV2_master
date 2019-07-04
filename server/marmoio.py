72c1674c334654b9039409c17cd1acdaec9f423c
Darren <darren.rajit@gmail.com>
2019-07-04 13:45:34 +1000

    fixes to json communication

--- server/marmoio.py
+++ server/marmoio.py
@@ -8,7 +8,7 @@ import datetime
 
 class marmoIO:
     # Configuration Constants:
-    marmobox_child_url = 'http://localhost:8000/'
+    marmobox_child_url = 'http://127.0.0.1:5000/'
 
     def __init__(self):
         print('marmoio intitiated. ')
@@ -28,17 +28,15 @@ class marmoIO:
         success_state = self.protocol.success_state()
         return success_state
 
-    def json_create(self,tasklist,animal_ID):
+    def json_create(self,taskname,animal_ID):
         #create json file into json_file folder for export to marmobox
         timestamp = datetime.datetime.now()
         print('Creating json and instructions for action by marmobox...')
 
         #creating dictionary file
-        json_input = {'animal_ID':animal_ID, 'tasklist':tasklist,
+        json_input = {"animal_ID":animal_ID, "taskname":taskname,
                       }
-
-        self.json_obj = json_obj = json.dumps(json_input)
-        print(json_obj)
+        json_obj = json.dumps(json_input)
         return json_obj
 
 
@@ -47,12 +45,12 @@ class marmoIO:
             Method to send an instruction to the child
         '''
         print('Sending json with instructions to marmobox... ')
+        print(json_obj)
         instructions = requests.post(self.marmobox_child_url, json=json_obj)#send json string to minipc and wait for response
         print('Post Request status code: ', instructions.status_code)
-        print('Sent instructions: ', instructions.body)
         print('Sent instructions: ', instructions.text)
-        self.__json_receive(response.json()) # Converts response to JSON and calls receive callback
-        
+        self.__json_on_response(instructions.json()) # Converts response to JSON and calls receive callback
+        #
 
     def __json_on_response(self,response):
         '''
